d4d2f46701e0c6e79bd4b79490848f8ab202239b
ross <ross@localhost.localdomain>
2026-04-08 22:48:30 -0400

    correccion

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -34,7 +34,7 @@ class ModelEvaluation:
              open(pred_path, "w", encoding="utf-8") as f_pred:
 
             for example in dataset:
-                inputs = example.to(device) #tokenizer(example['sentence'], return_tensors="pt", truncation=True).to(device)
+                inputs = example #tokenizer(example['sentence'], return_tensors="pt", truncation=True).to(device)
                 
                 with torch.no_grad():
                     output_tokens = model.generate(
